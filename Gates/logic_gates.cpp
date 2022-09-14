5ad5f6a257212c3b7b17438c6acf61f7c662981d
Arseny Bulaev <seni190999@mail.ru>
2022-09-14 18:26:41 +0300

    Namespace logic gates

--- Gates/logic_gates.cpp
+++ Gates/logic_gates.cpp
@@ -1,18 +1,17 @@
+#include "logic_gates.h"
 
 
-
-
-bool and(bool input1, bool input2){
+bool logic_gates::and_gate(bool input1, bool input2){
     return input1 && input2; 
 }
 
-bool or(bool input1, bool input2){
+bool logic_gates::or_gate(bool input1, bool input2){
     return input1 || input2; 
 }
-bool and_not(bool input1, bool input2){
+bool logic_gates::and_not_gate(bool input1, bool input2){
     return !(input1 && input2); 
 }
 
-bool or_not(bool input1, bool input2){
+bool logic_gates::or_not_gate(bool input1, bool input2){
     return !(input1 || input2); 
 }
--- Gates/logic_gates.h
+++ Gates/logic_gates.h
@@ -1,7 +1,7 @@
 
-
-
-bool and(bool, bool);
-bool or(bool, bool);
-bool and_not(bool, bool);
-bool or_not(bool, bool);
+namespace logic_gates{
+    bool and_gate(bool, bool);
+    bool or_gate(bool, bool);
+    bool and_not_gate(bool, bool);
+    bool or_not_gate(bool, bool);
+}
